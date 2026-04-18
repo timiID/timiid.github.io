067f4286a56786d48714c9147fb1bdfc0aeb2aa7
d <252406691+timiID@users.noreply.github.com>
2026-04-18 15:34:38 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -93,9 +93,9 @@ footer::before {
   width: 100%;
   height: 100%;
   background-image: url('/images/what chara.gif');
-  backgroundRepeat: 'no-repeat';
-  backgroundPosition: 'right top';
-  backgroundAttachment: 'scroll';
+  background-repeat: no-repeat;
+  background-position: right top;
+  background-attachment: scroll;
   background-size: auto 100%;      
   opacity: 0.12;        
   z-index: 1;
