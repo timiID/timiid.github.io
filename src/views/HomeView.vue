2e23540e4629d124bd7660096d855f7f7bea0bc6
d <timikagenou@gmail.com>
2026-08-24 11:08:14 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -321,7 +321,7 @@ onUnmounted(() => { if (homeInterval) clearInterval(homeInterval); });
 </button>
       </div>
       
-      <div v-if="favoriteXtalls.length" class="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
+      <div v-if="favoriteXtalls.length" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
         <button 
           v-for="favorite in favoriteXtalls" 
           :key="favorite.code" 
